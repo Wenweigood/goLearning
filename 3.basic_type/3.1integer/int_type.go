acb045320e245f7c5b15f0e7c06a05f5596a4c7e
wv <923660344@qq.com>
2024-08-31 14:17:32 +0800

    整型

---
+++ 3.basic_type/3.1integer/int_type.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	newstring := "123,456"
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)   // "97 a 'a'"
+	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 国 '国'"
+	fmt.Printf("%d %[1]q\n", newline)       // "10 '\n'"
+	fmt.Printf("%s\n", newstring)
+}
